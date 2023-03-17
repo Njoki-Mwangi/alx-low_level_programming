a32f8f46bde96dce3eaecd3d9e0f91cd7adf553f
Njoki <faithgracem96@gmail.com>
2023-03-17 22:56:12 +0000

    andequal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 				}
 				else
 				{
-					printf("Last digit of  %d is %d and is 0\n", n, ld);
+					printf("Last digit of %d is %d and is 0\n", n, ld);
 				}
 
 					return (0);
