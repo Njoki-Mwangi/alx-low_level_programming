e37b9f2d71a2537bc7a9c7255faf8dc8a1f7a23c
Njoki <faithgracem96@gmail.com>
2023-03-18 00:24:58 +0000

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main-entry pointt
+ *
+ * Description:prints lower cas
+ * e
+ * Returns:Always 0 (succes)
+ */
+int main(void)
+
+{
+char ch;
+
+for i(ch = 'a' ; ch <= 'z' ; ch++);
+
+{
+
+putchar (ch);
+
+putchar('\n');
+
+}
+
+return (0);
+}
