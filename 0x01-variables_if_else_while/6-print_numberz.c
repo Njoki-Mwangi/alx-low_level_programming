a5de32d8cca554eccafd0d8cdfd8211cdbc28e45
Njoki <faithgracem96@gmail.com>
2023-03-18 13:00:06 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Description -print numbers
+ *
+ * Return - Always 0(sucess)
+ */
+int main()
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar((num % 10) + '0');
+putchar('\n');
+}
+return (0);
+}
