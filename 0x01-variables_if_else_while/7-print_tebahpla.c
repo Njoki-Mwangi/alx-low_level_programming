8d92bbe0e5f53c70b98c548d7816c4af91c9e534
Njoki <faithgracem96@gmail.com>
2023-03-20 20:42:33 +0000

    print alphabets in lowercase rv

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:print variabbles in lowercase
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+
+putchar (ch);
+putchar('\n');
+
+return (0);
+
+}
