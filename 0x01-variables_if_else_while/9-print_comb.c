bbea7a099deef160eaab32dc06ceebc5915091ee
Njoki <faithgracem96@gmail.com>
2023-03-20 23:48:43 +0000

    printcomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:combinations of single_digit numbers
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+
+return (0);
+}
+
