3d00a8c1f392383f799193066c41fae90cb839d9
Njoki <faithgracem96@gmail.com>
2023-03-22 00:52:31 +0000

    1-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
+ *  Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
