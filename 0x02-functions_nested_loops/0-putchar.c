e73c44f93909665de4795be340cb9b9c56976a6d
Njoki <faithgracem96@gmail.com>
2023-03-22 00:22:28 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: print _putchar
+*Return:Always 0 (success)
+*/
+int main(void)
+
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
