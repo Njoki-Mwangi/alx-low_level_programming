d23be8768998dbb266bae889fe9e939d7a48d88c
Njoki <faithgracem96@gmail.com>
2023-03-29 20:51:40 +0000

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
 
+char *leet(char *);
+void reverse_array(int *a, int n);
+char *cap_string(char *);
 char *_strcat(char *dest, char *src);
-
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
