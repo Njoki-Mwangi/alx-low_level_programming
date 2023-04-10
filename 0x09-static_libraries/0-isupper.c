48bc49cbefbe0002aa4f2f1d48d7dc298f3cad1d
Njoki <faithgracem96@gmail.com>
2023-04-10 20:41:16 +0000

    A library is not a luxury

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - a function that checks for uppercase character
+*@c : input
+*Return: 1 if c is uppercase otherwise return 0
+*/
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
+}
+
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* _memset - fills the first n bytes
+*@s: memory area
+*@b: constant bytes
+*@n:  number of bytes
+*Return: s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+
+s[i] = b;
+
+}
+
+return (s);
+
+}
